5e739a31e5203560cd9a33968f7febfdb7b3c9b2
Alexander <37180898+MrSago@users.noreply.github.com>
2020-12-19 15:20:43 +0300

    Add other sort algorithms and test's result

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -1,14 +1,13 @@
 
 import sort
-from prettytable import PrettyTable
-from time import time
 from random import randint
+from prettytable import PrettyTable
 
 
 def initTestArrays(N):
     arr = [[], [], []]
     arr[0] = [int(i) for i in range(1, N + 1, 1)]
-    for i in range(0, N): arr[1].append(randint(-1000000, 1000000))
+    for i in range(0, N): arr[1].append(randint(-1000000000, 1000000000))
     arr[2] = [int(i) for i in range(N, 0, -1)]
     return arr
 
@@ -19,6 +18,19 @@ def copyTestArrays(arr):
     new_arr[2] = arr[2].copy()
     return new_arr
 
+def resTestFun(fun, test):
+    input = copyTestArrays(test)
+    ls = ["%0.8f" % round(fun(input[0]), 8)]
+    if (not sort.Check(input[0])):
+        raise Exception("input[0] not sorted in function: %s" % fun.__name__)
+    ls += ["%0.8f" % round(fun(input[1]), 8)]
+    if (not sort.Check(input[1])):
+        raise Exception("input[1] not sorted in function: %s" % fun.__name__)
+    ls += ["%0.8f" % round(fun(input[2]), 8)]
+    if (not sort.Check(input[2])):
+        raise Exception("input[2] not sorted in function: %s" % fun.__name__)
+    return ls
+
 def startSort():
     N = int(input())
     test_arrays = initTestArrays(N)
@@ -26,33 +38,13 @@ def startSort():
     table = PrettyTable()
     table.field_names = ["№", "Метод", "Отсортированная", "Случайная", "В обратом порядке"]
 
-    #Python Sort
-    input_arrays = copyTestArrays(test_arrays)
-    table.add_row(["0", "Python Sort",
-                   "%0.8f" % round(sort.pySort(input_arrays[0]), 8),
-                   "%0.8f" % round(sort.pySort(input_arrays[1]), 8),
-                   "%0.8f" % round(sort.pySort(input_arrays[2]), 8)])
-
-    #Bubble Sort
-    input_arrays = copyTestArrays(test_arrays)
-    table.add_row(["1", "Bubble Sort",
-                   "%0.8f" % round(sort.bubbleSort(input_arrays[0]), 8),
-                   "%0.8f" % round(sort.bubbleSort(input_arrays[1]), 8),
-                   "%0.8f" % round(sort.bubbleSort(input_arrays[2]), 8)])
-
-    #Insertion Sort
-    input_arrays = copyTestArrays(test_arrays)
-    table.add_row(["2", "Insertion Sort",
-                   "%0.8f" % round(sort.insertionSort(input_arrays[0]), 8),
-                   "%0.8f" % round(sort.insertionSort(input_arrays[1]), 8),
-                   "%0.8f" % round(sort.insertionSort(input_arrays[2]), 8)])
-
-    #Quick Sort
-    input_arrays = copyTestArrays(test_arrays)
-    table.add_row(["6", "Quick Sort",
-                   "%0.8f" % round(sort.quickSort(input_arrays[0]), 8),
-                   "%0.8f" % round(sort.quickSort(input_arrays[1]), 8),
-                   "%0.8f" % round(sort.quickSort(input_arrays[2]), 8)])
+    table.add_row(["0", "Python Sort"] + resTestFun(sort.pySort, test_arrays))
+    table.add_row(["1", "Bubble Sort"] + resTestFun(sort.bubbleSort, test_arrays))
+    table.add_row(["2", "Insertion Sort"] + resTestFun(sort.insertionSort, test_arrays))
+    table.add_row(["3", "Selection Sort"] + resTestFun(sort.selectionSort, test_arrays))
+    table.add_row(["4", "Merge Sort"] + resTestFun(sort.mergeSort, test_arrays))
+    table.add_row(["5", "Shell's Sort"] + resTestFun(sort.shellSort, test_arrays))
+    table.add_row(["6", "Quick Sort"] + resTestFun(sort.quickSort, test_arrays))
 
     print("Количество элементов: %d" % N)
     print(table)
--- lab8/sort.py
+++ lab8/sort.py
@@ -4,10 +4,6 @@ from time import time
 def Check(A):
     return all(A[i] <= A[i + 1] for i in range(len(A) - 1))
 
-#========================
-#====== Variant #1 ======
-#========================
-
 #Method #0
 #Python Sort
 def pySort(ptrArray):
@@ -45,6 +41,66 @@ def insertionSort(ptrArray):
         ptrArray[j + 1] = tmp
     return time() - start
 
+#Method 3
+#Selection Sort
+def selectionSort(ptrArray):
+    start = time()
+    for i, val in enumerate(ptrArray):
+        select = min(range(i, len(ptrArray)), key=ptrArray.__getitem__)
+        ptrArray[i], ptrArray[select] = ptrArray[select], val
+    return time() - start
+
+#Method 4
+#Merge Sort
+def mergeSort(ptrArray):
+    def recMs(arr):
+        count = len(arr)
+        if (count > 2):
+            left = recMs(arr[:count // 2])
+            right = recMs(arr[count // 2:])
+            arr = left + right
+
+            last_i = len(arr) - 1
+            for i in range(last_i):
+                min_val = arr[i]
+                min_i = i
+                for j in range(i + 1, last_i + 1):
+                    if min_val > arr[j]:
+                        min_val = arr[j]
+                        min_i = j
+                if (min_i != i):
+                    arr[i], arr[min_i] = arr[min_i], arr[i]
+        elif (count == 2 and arr[0] > arr[1]):
+            arr[0], arr[1] = arr[1], arr[0]
+
+        return arr
+
+
+    start = time()
+    tmp = recMs(ptrArray)
+    stop = time()
+
+    for i in range(len(tmp)):
+        ptrArray[i] = tmp[i]
+    return stop - start
+
+#Method 5
+#Shell's Sort
+def shellSort(ptrArray):
+    start = time()
+    last = len(ptrArray)
+    step = last // 2
+    while (step > 0):
+        for i in range(step, last):
+            tmp = ptrArray[i]
+            j = i
+            while (j >= step and ptrArray[j - step] > tmp):
+                ptrArray[j] = ptrArray[j - step]
+                j -= step
+            ptrArray[j] = tmp
+        step //= 2
+    return time() - start
+
 #Method #6
 #Quick Sort
 def quickSort(ptrArray):
@@ -66,8 +122,13 @@ def quickSort(ptrArray):
     def recQs(arr, low, high):
         while (low < high):
             p = partition(arr, low, high)
-            recQs(arr, low, p - 1)
-            low = p + 1
+            if (p - low < high - p):
+                recQs(arr, low, p)
+                low = p + 1
+            else:
+                recQs(arr, p + 1, high)
+                high = p
+
 
     start = time()
     recQs(ptrArray, 0, len(ptrArray) - 1)
