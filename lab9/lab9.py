f8a76784556f9b59f80b7dedb57f461d4bc30a51
Alexander <37180898+MrSago@users.noreply.github.com>
2021-01-02 15:12:19 +0300

    Add lab9

---
+++ lab9/lab9.py
@@ -0,0 +1,144 @@
+
+import sort
+import numpy
+from random import randint
+from math import ceil
+from time import time
+from warnings import filterwarnings
+filterwarnings("error")
+
+#======Variant #1======
+inpFn = "input.txt"
+N = 50000
+P = 4
+M = 60
+funSort = sort.bubbleSort
+#======================
+
+def generateRandomArray(count):
+    with open(inpFn, "w") as f:
+        for i in range(0, count):
+            f.write(str(randint(-1000000, 1000000)) + '\n')
+
+def createFiles(N, M):
+    flag = True
+    A = open("A.txt", "w")
+    B = open("B.txt", "w")
+    curFile = A
+    for i in range(0, ceil(N / M)):
+        arr = numpy.loadtxt(inpFn, numpy.int, skiprows=i*M, max_rows=M)
+        funSort(arr)
+        numpy.savetxt(curFile, arr, "%i", delimiter='\n')
+        if (flag):
+            curFile = B
+            flag = False
+        else:
+            curFile = A
+            flag = True
+    A.close()
+    B.close()
+
+def checkIsSortedFile(fName):
+    return sort.Check(numpy.loadtxt(fName, numpy.int))
+
+def mergeFiles(input1, input2, output, size):
+    flag = False
+    arr1 = arr2 = []
+
+    try:
+        arr1 = numpy.loadtxt(input1, numpy.int, max_rows=M//2).tolist()
+        arr2 = numpy.loadtxt(input2, numpy.int, max_rows=M//2).tolist()
+        while (len(arr1) > 0 and len(arr2) > 0):
+            if (arr1[0] < arr2[0]):
+                output.write(str(arr1.pop(0)) + '\n')
+            else:
+                output.write(str(arr2.pop(0)) + '\n')
+        for i in range(0, size - 1):
+            if (len(arr1) == 0):
+                arr1 = numpy.loadtxt(input1, numpy.int, max_rows=M//2).tolist()
+            else:
+                arr2 = numpy.loadtxt(input2, numpy.int, max_rows=M//2).tolist()
+
+            while (len(arr1) > 0 and len(arr2) > 0):
+                if (arr1[0] < arr2[0]):
+                    output.write(str(arr1.pop(0)) + '\n')
+                else:
+                    output.write(str(arr2.pop(0)) + '\n')
+    except Exception:
+        flag = True
+
+    try:
+        if (len(arr1) != 0):
+            numpy.savetxt(output, arr1, "%i", delimiter='\n')
+            if (flag):
+                numpy.savetxt(output, numpy.loadtxt(input1, numpy.int), "%i", delimiter='\n')
+        else:
+            numpy.savetxt(output, arr2, "%i", delimiter='\n')
+            if (flag):
+                numpy.savetxt(output, numpy.loadtxt(input2, numpy.int), "%i", delimiter='\n')
+    except Exception:
+        pass
+
+    return flag
+
+def polyPhaseMerge(M):
+    size = M
+    fnA = "A.txt"
+    fnB = "B.txt"
+    fnC = "C.txt"
+    fnD = "D.txt"
+    modeAB = "r"
+    modeCD = "w"
+    lastOutFn = ""
+    flag = True
+
+    while (size < N):
+        A = open(fnA, modeAB)
+        B = open(fnB, modeAB)
+        C = open(fnC, modeCD)
+        D = open(fnD, modeCD)
+        modeAB, modeCD = modeCD, modeAB
+        if (flag):
+            input1 = A
+            input2 = B
+            curOut = C
+            lastOutFn = fnC
+            flag = False
+        else:
+            input1 = C
+            input2 = D
+            curOut = A
+            lastOutFn = fnA
+            flag = True
+
+        while (True):
+            if (mergeFiles(input1, input2, curOut, ((size * 2) // (M // 2)) - 1)):
+                break
+            if (curOut == C):
+                curOut = D
+            elif (curOut == D):
+                curOut = C
+            elif (curOut == A):
+                curOut = B
+            elif (curOut == B):
+                curOut = A
+
+        A.close()
+        B.close()
+        C.close()
+        D.close()
+        size *= 2
+
+    return lastOutFn
+
+
+generateRandomArray(N)
+start = time()
+createFiles(N, M)
+fNameResult = polyPhaseMerge(M)
+stop = time()
+
+print("Input file: %s" % inpFn)
+print("Result in file: %s" % fNameResult)
+print("Time sorting: %f" % (stop - start))
+print("Is sorted: %r" % checkIsSortedFile(fNameResult))
---
+++ lab9/sort.py
@@ -0,0 +1,135 @@
+
+from time import time
+
+def Check(A):
+    return all(A[i] <= A[i + 1] for i in range(len(A) - 1))
+
+#Method #0
+#Python Sort
+def pySort(ptrArray):
+    start = time()
+    ptrArray.sort()
+    return time() - start
+
+#Method #1
+#Bubble Sort
+def bubbleSort(ptrArray):
+    start = time()
+    for i in range(len(ptrArray) - 1):
+        isSwap = False
+        for j in range(len(ptrArray) - 1 - i):
+            first = ptrArray[j]
+            second = ptrArray[j + 1]
+            if (first > second):
+                ptrArray[j] = second
+                ptrArray[j + 1] = first
+                isSwap = True
+        if (not isSwap):
+            break
+    return time() - start
+
+#Method #2
+#Insertion Sort
+def insertionSort(ptrArray):
+    start = time()
+    for i in range(1, len(ptrArray)):
+        tmp = ptrArray[i]
+        j = i - 1
+        while (j >= 0 and ptrArray[j] > tmp):
+            ptrArray[j + 1] = ptrArray[j]
+            j -= 1
+        ptrArray[j + 1] = tmp
+    return time() - start
+
+#Method 3
+#Selection Sort
+def selectionSort(ptrArray):
+    start = time()
+    for i, val in enumerate(ptrArray):
+        select = min(range(i, len(ptrArray)), key=ptrArray.__getitem__)
+        ptrArray[i], ptrArray[select] = ptrArray[select], val
+    return time() - start
+
+#Method 4
+#Merge Sort
+def mergeSort(ptrArray):
+    def recMs(arr):
+        count = len(arr)
+        if (count > 2):
+            left = recMs(arr[:count // 2])
+            right = recMs(arr[count // 2:])
+            arr = left + right
+
+            last_i = len(arr) - 1
+            for i in range(last_i):
+                min_val = arr[i]
+                min_i = i
+                for j in range(i + 1, last_i + 1):
+                    if min_val > arr[j]:
+                        min_val = arr[j]
+                        min_i = j
+                if (min_i != i):
+                    arr[i], arr[min_i] = arr[min_i], arr[i]
+        elif (count == 2 and arr[0] > arr[1]):
+            arr[0], arr[1] = arr[1], arr[0]
+
+        return arr
+
+
+    start = time()
+    tmp = recMs(ptrArray)
+    stop = time()
+
+    for i in range(len(tmp)):
+        ptrArray[i] = tmp[i]
+    return stop - start
+
+#Method 5
+#Shell's Sort
+def shellSort(ptrArray):
+    start = time()
+    last = len(ptrArray)
+    step = last // 2
+    while (step > 0):
+        for i in range(step, last):
+            tmp = ptrArray[i]
+            j = i
+            while (j >= step and ptrArray[j - step] > tmp):
+                ptrArray[j] = ptrArray[j - step]
+                j -= step
+            ptrArray[j] = tmp
+        step //= 2
+    return time() - start
+
+#Method #6
+#Quick Sort
+def quickSort(ptrArray):
+    def partition(arr, low, high):
+        pi = arr[(low + high) // 2]
+        i = low - 1
+        j = high + 1
+        while (True):
+            i += 1
+            while (arr[i] < pi):
+                i += 1
+            j -= 1
+            while (arr[j] > pi):
+                j -= 1
+            if (i >= j):
+                return j
+            arr[i], arr[j] = arr[j], arr[i]
+
+    def recQs(arr, low, high):
+        while (low < high):
+            p = partition(arr, low, high)
+            if (p - low < high - p):
+                recQs(arr, low, p)
+                low = p + 1
+            else:
+                recQs(arr, p + 1, high)
+                high = p
+
+
+    start = time()
+    recQs(ptrArray, 0, len(ptrArray) - 1)
+    return time() - start
